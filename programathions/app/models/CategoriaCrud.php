28abba76a328eecb33ad11de5339f03dbb05299a
geancarlossfs <torinoge@gmail.com>
2018-03-13 16:54:04 -0300

    Add files via upload

--- programathions/app/models/CategoriaCrud.php
+++ programathions/app/models/CategoriaCrud.php
@@ -1,17 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aluno
- * Date: 06/03/18
- * Time: 16:01
- */
+
 
 require_once 'DBConnection.php';
 require_once 'Categoria.php';
 
 class CategoriaCrud{
     //SEMPRE QUE A CLASSE FOR INSTANCIADA, JA FAZ A CONEXÃO E GUARDA
-    
     private $conexao;
     public function __construct(){
         $this->conexao = DBConnection::getConexao();
@@ -24,26 +18,23 @@ class CategoriaCrud{
         $sql ='select * from categoria where id_categoria='.$id;
         $resultado = $this->conexao->query($sql);
 
-
         //FETCH - TRANSFORMA O RESULTADO EM UM ARRAY ASSOCIATIVO
         $categoria = $resultado->fetch(PDO::FETCH_ASSOC);
-
         //CRIAR OBJETO DO TIPO CATEGORIA - USANDO OS VALORES DA CONSULTA
         $objcat = new Categoria($categoria['id_categoria'], $categoria['nome_categoria'], $categoria['descricao_categoria']);
 
         //RETORNAR UM OBJETO CATEGORIA COM OS VALORES
-        //return $objcat;
+        return $objcat;
 
     }
 
     public function getCategorias(){
         $sql = "SELECT * FROM categoria";
 
-        $resultado = $this->query($sql);
+        $resultado = $this->conexao->query($sql);
 
         $categorias = $resultado->fetchAll(PDO::FETCH_ASSOC);
 
-        $listaCategorias = [];
         foreach ($categorias as $categoria){
             $id = $categoria['id_categoria'];
             $nome = $categoria['nome_categoria'];
@@ -51,15 +42,40 @@ class CategoriaCrud{
 
             $obj = new Categoria($id, $nome, $descricao);
             $listaCategorias[] = $obj;
-
-
         }
-
         return $listaCategorias;
-        //var_dump($categorias);
-
-
     }
 
-    //FAZER TESTES DAS FUNÇÕES NO ARQUIVO DE TESTES
+    public function insertCategoria(Categoria $cat){
+
+        $sql = "INSERT INTO categoria (nome_categoria, descricao_categoria) VALUES ('".$cat->getNome()."','".$cat->getDescricao()."')";
+
+        try{
+            $this->conexao->exec($sql);
+        }catch (PDOException $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function upadateCategoria(Categoria $cat){
+        // MONTA O TEXTO
+        $sql = "UPDATE categoria SET (nome_categoria = '".$cat->getNome()."', descricao_categoria = '".$cat->getDescricao()."') WHERE id_categoria =".$cat->getId();
+
+        try{
+            $this->conexao->exec($sql);
+        }catch (PDOException $e){ //EM CASO DE ERRO, CAPTURA E RTETORNA A MENSAGEM
+            return $e->getMessage();
+        }
+    }
+
+    public function deleteCategoria(int $id){
+
+        $sql = "DELETE FROM categoria WHERE id_categoria=5";
+
+        try{ //TENTA EXECUTAR A INSTRUÇÃO
+            $this->conexao->exec($sql);
+        }catch (PDOException $e){
+            return $e;
+        }
+    }
 }
